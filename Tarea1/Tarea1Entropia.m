5606d8bf83dd3ffa667e21a41d4c7057e7bf0e59
Ivan Trejo <ivantm11@icloud.com>
2021-03-07 18:53:21 -0600

    UPDATE: code clean and rename pdf files for read

--- Tarea1/Tarea1Entropia.m
+++ Tarea1/Tarea1Entropia.m
@@ -3,9 +3,9 @@ clc; clear; close all;
 alfabeto = categorical({'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'});
 alfabeto_esp = categorical({'A','B','C','D','E','F','G','H','I','J','K','L','M','N','N_','O','P','Q','R','S','T','U','V','W','X','Y','Z'});
 
-libro_esp = 'libros/66paginas.pdf';
-libro_eng = 'libros/134ingles.pdf';
-libro_fra = 'libros/94fra.pdf';
+libro_esp = 'libros/libro-esp.pdf';
+libro_eng = 'libros/libro-eng.pdf';
+libro_fra = 'libros/libro-fra.pdf';
 
 %% Idioma español
 
@@ -26,7 +26,7 @@ end
 espanol_entropia = sum(espanolv_entro); % Valor de la entropia
 
 % Tabla de probabilidades con entropia
-tabla_espanol = table(alfabeto_esp',p_espanol',espanolauto', 'VariableNames',{'Simbolo','Probabilidad','Autoinfo'})
+tabla_espanol = table(alfabeto_esp', p_espanol', espanolauto', 'VariableNames', {'Simbolo', 'Probabilidad', 'Autoinfo'})
 
 %% Idioma inglés
 
@@ -47,7 +47,7 @@ end
 ingles_entropia = sum(inglesv_entro); % Valor de la entropia
 
 % Tabla de probabilidades con entropia
-tabla_ingles = table(alfabeto',p_ingles',inglesauto', 'VariableNames',{'Simbolo','Probabilidad','Autoinfo'})
+tabla_ingles = table(alfabeto', p_ingles', inglesauto', 'VariableNames', {'Simbolo', 'Probabilidad', 'Autoinfo'})
 
 %% Idioma francés
 
@@ -68,4 +68,4 @@ end
 frances_entropia = sum(francesv_entro); % Valor de la entropia
 
 % Tabla de probabilidades con entropia
-tabla_frances = table(alfabeto',p_frances',francesauto', 'VariableNames',{'Simbolo','Probabilidad','Autoinfo'})
+tabla_frances = table(alfabeto', p_frances', francesauto', 'VariableNames', {'Simbolo', 'Probabilidad', 'Autoinfo'})
--- Tarea1/obtener_probabilidades_esp.m
+++ Tarea1/obtener_probabilidades_esp.m
@@ -20,7 +20,7 @@ function [vector] = obtener_probabilidades_esp(archivo)
     % ' !"#$%&'()*+,-./' ASCII(32-47)
     for r = 32:47
         simbolo = char(r);                              % Convirtiendo de código ASCII a símbolo.
-        total = length(strfind(letrasUpper,simbolo));   % Buscando el numero de ocurrencias
+        total = length(strfind(letrasUpper, simbolo));   % Buscando el numero de ocurrencias
         c_especiales = c_especiales + total;            % Aumentando el contador de especiales
         total = 0;                                      % Limpiando el contador para el proximo simbolo
     end
@@ -28,7 +28,7 @@ function [vector] = obtener_probabilidades_esp(archivo)
     % ':;<=>?@' ASCII(58-64)
     for r = 58:64
         simbolo = char(r);                              % Convirtiendo de código ASCII a símbolo.
-        total = length(strfind(letrasUpper,simbolo));   % Buscando el numero de ocurrencias
+        total = length(strfind(letrasUpper, simbolo));   % Buscando el numero de ocurrencias
         c_especiales = c_especiales + total;            % Aumentando el contador de especiales
         total = 0;                                      % Limpiando el contador para el proximo simbolo
     end    
@@ -36,7 +36,7 @@ function [vector] = obtener_probabilidades_esp(archivo)
     % '[\]^_`' ASCII(91-96)
     for r = 91:96
         simbolo = char(r);                              % Convirtiendo de código ASCII a símbolo.
-        total = length(strfind(letrasUpper,simbolo));   % Buscando el numero de ocurrencias
+        total = length(strfind(letrasUpper, simbolo));   % Buscando el numero de ocurrencias
         c_especiales = c_especiales + total;            % Aumentando el contador de especiales
         total = 0;                                      % Limpiando el contador para el proximo simbolo
     end    
@@ -48,30 +48,30 @@ function [vector] = obtener_probabilidades_esp(archivo)
         % Caso â = ä = à = å = Ä = Å = á = Á = Â = À = ã = Ã
         if(r == 131 || r == 132 || r == 133 || r == 134 || r == 142 || r == 143 || r == 160 || r == 181 || r == 182 || r == 183 || r == 198 || r == 199)
             otro = char(r);
-            tipos_a = tipos_a + length(strfind(letrasUpper,otro)); % Buscando el numero de ocurrencias
+            tipos_a = tipos_a + length(strfind(letrasUpper, otro)); % Buscando el numero de ocurrencias
         % Caso é = ê = ë = è = É = Ê = Ë = È
         elseif(r ==130 || r == 136 || r == 137 || r ==138 || r == 144 || r == 210 || r == 211 || r == 212)
             otro = char(r);
-            tipos_e = tipos_e + length(strfind(letrasUpper,otro)); % Buscando el numero de ocurrencias
+            tipos_e = tipos_e + length(strfind(letrasUpper, otro)); % Buscando el numero de ocurrencias
         % Caso ï = î = ì = í = Í = Î = Ï
         elseif(r == 139 || r == 140 || r ==141 || r == 161 || r == 214 || r == 215 || r == 216)
             otro = char(r);
-            tipos_i = tipos_i + length(strfind(letrasUpper,otro)); % Buscando el numero de ocurrencias
+            tipos_i = tipos_i + length(strfind(letrasUpper, otro)); % Buscando el numero de ocurrencias
         % Caso ô = ö = ò = Ö = ø = Ø = ó = Ó = Ô = Ò = õ = Õ
         elseif(r == 147 || r == 148 || r == 149 || r == 153 || r == 155 || r == 157 || r == 162 || r == 224 || r == 226 || r == 227 || r == 228 || r == 229)
             otro = char(r);
-            tipos_o = tipos_o + length(strfind(letrasUpper,otro)); % Buscando el numero de ocurrencias
+            tipos_o = tipos_o + length(strfind(letrasUpper, otro)); % Buscando el numero de ocurrencias
         % Caso ü = û = ù = Ü = ú = Ú = Û = Ù
         elseif(r == 129 || r ==150 || r == 151 || r == 154 || r == 163 || r == 233 || r == 234 || r == 235)
             otro = char(r);
-            tipos_u = tipos_u + length(strfind(letrasUpper,otro)); % Buscando el numero de ocurrencias
+            tipos_u = tipos_u + length(strfind(letrasUpper, otro)); % Buscando el numero de ocurrencias
         % Caso de la ñ O Ñ
         elseif(r == 209 || r == 241)
             otro = char(r); 
-            total_n = total_n + length(strfind(letrasUpper,otro)); % Buscando el numero de ocurrencias(ñ)
+            total_n = total_n + length(strfind(letrasUpper, otro)); % Buscando el numero de ocurrencias(ñ)
         else   
             simbolo = char(r);                              % Convirtiendo de código ASCII a símbolo.
-            total = length(strfind(letrasUpper,simbolo));   % Buscando el numero de ocurrencias
+            total = length(strfind(letrasUpper, simbolo));   % Buscando el numero de ocurrencias
             c_especiales = c_especiales + total;            % Aumentando el contador de especiales
             total = 0;                                      % Limpiando el contador para el proximo simbolo
         end
@@ -83,7 +83,7 @@ function [vector] = obtener_probabilidades_esp(archivo)
     n = 1;  % Indice para letras del alfabeto en el resultado devuelto
     for j = 65:90 % Contabilizando mayusculas A-Z
         letra = char(j);                                % Convirtiendo el inidice del for a Caracter (ASCII-Letra)
-        total = length(strfind(letrasUpper,letra));     % Buscando el numero de ocurrencias
+        total = length(strfind(letrasUpper, letra));     % Buscando el numero de ocurrencias
         
         % Agregamos las variaciones de las vocales
         % Caso A
